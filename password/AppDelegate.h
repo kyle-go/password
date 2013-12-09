071df397496c716d80d67a664dbd1469e43997b4
wangkangan <kyle.script@gmail.com>
2013-12-09 11:45:53 +0800

    上传工程代码

---
+++ password/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  password
+//
+//  Created by kyle on 13-12-9.
+//  Copyright (c) 2013年 kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ password/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  password
+//
+//  Created by kyle on 13-12-9.
+//  Copyright (c) 2013年 kyle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
